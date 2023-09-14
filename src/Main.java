23fde4b36f2641faaed604e124dd90f0f9835770
Miles  Cresci <milescresci@miless-air.wifi.uc.edu>
2023-09-14 15:14:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //variables
+
+        int PurchasePrice = 10;
+        double salesTax = 1.05;
+
+        double totalCost = PurchasePrice * salesTax;
+        System.out.println(" The total cost is " + totalCost + " and the sales tax is 5% ");
+
+    }
+}
